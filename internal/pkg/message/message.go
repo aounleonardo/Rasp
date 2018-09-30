d574fe96b29ba235e882fcd22264b2dbbbe896b0
Leonardo Aoun <leonardo.aoun@epfl.ch>
2018-09-30 15:45:51 +0200

    create rumor and status message structs

--- internal/pkg/message/message.go
+++ internal/pkg/message/message.go
@@ -6,10 +6,27 @@ type SimpleMessage struct {
 	Contents string
 }
 
+type RumorMessage struct {
+	Origin string
+	ID uint32
+	Text string
+}
+
 type GossipPacket struct {
 	Simple *SimpleMessage
+	Rumor *RumorMessage
+	Status *StatusPacket
 }
 
 type ClientPacket struct {
 	Message string
+}
+
+type PeerStatus struct {
+	Identifier string
+	NextID uint32
+}
+
+type StatusPacket struct {
+	Want []PeerStatus
 }
