76234855e1d1891641bea8f72f9a999db53916a4
Leonardo Aoun <leonardo.aoun@epfl.ch>
2018-12-24 00:31:43 +0100

    state balance

---
+++ www/src/components/Challenge.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Challenge extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Challenge
+            </div>
+        );
+    }
+}
+
+export default Challenge;
--- www/src/components/State.js
+++ www/src/components/State.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
 import colors from "./colors";
+import Challenge from "./Challenge";
+
+const challengeStates = [
+    "Proposed",
+    "Pending",
+    "Accepted",
+    "Ongoing",
+    "Finished",
+];
 
 export default class State extends Component {
     constructor(props) {
@@ -9,15 +18,66 @@ export default class State extends Component {
     render() {
         return (
             <div style={styles.state}>
-                {this.props.name}
+                <div style={styles.playerContainer}>
+                    <div style={styles.playerLabels}>
+                        {this.props.name}
+                    </div>
+                    <div style={styles.playerLabels}>
+                        {this.props.balance}
+                    </div>
+                </div>
+                <div style={styles.scrollContainer}>
+                    {challengeStates.map((state) => (
+                        <Set
+                            key={state}
+                            name={state}
+                            challenges={[]}
+                            clickCallback={() => {}}
+                        />
+                    ))}
+                </div>
             </div>
         )
     }
 }
 
+const Set = ({name, challenges, clickCallback}) => {
+    return (
+        <div>
+            <div>
+                {`${name}`}
+            </div>
+            {challenges.forEach((challenge, index) => (
+                <Challenge
+                    key={`${name}:${index}`}
+                    challenge={challenge}
+                    primaryColor={index % 2 === 0}
+                    onClick={clickCallback}
+                />
+            ))}
+        </div>
+    );
+};
+
 const styles = {
     state: {
         width: '100%',
+        backgroundColor: colors.beige,
+    },
+    playerContainer: {
+        display: 'flex',
+        height: 80,
         backgroundColor: colors.lightBlue,
     },
+    playerLabels: {
+        display: 'flex',
+        flex: 1,
+        color: colors.blue,
+        fontSize: 28,
+        fontWeight: 'bold',
+        fontFamily: "Helvetica",
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    scrollContainer: {},
 };
